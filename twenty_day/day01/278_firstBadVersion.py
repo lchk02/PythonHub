a8c9ce499a605c45b20f497804cc0fd799c765e8
xhw_587369 <xiang09@vip.qq.com>
2021-10-17 21:24:42 +0800

    初级算法day01

---
+++ twenty_day/day01/278_firstBadVersion.py
@@ -0,0 +1,58 @@
+""" 278. 第一个错误的版本
+你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。
+由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+
+假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+
+你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。
+实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+
+示例 1：
+输入：n = 5, bad = 4
+输出：4
+解释：
+调用 isBadVersion(3) -> false 
+调用 isBadVersion(5) -> true 
+调用 isBadVersion(4) -> true
+所以，4 是第一个错误的版本。
+
+示例 2：
+输入：n = 1, bad = 1
+输出：1
+"""
+
+import random
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+
+def is_bad_version(version):
+    # bad_version = random.randint(1, 10)
+    # print("bad_version: ", bad_version)
+    bad_version = 5
+    print("API被调用")
+    return version >= bad_version
+
+
+def first_bad_version(n):
+    left = 1
+    right = n
+    if is_bad_version(left):  # 第一个版本是错误的版本，直接返回
+        return left
+    elif bool(1-is_bad_version(right)):  # 最后一个版本是正确的版本，不存在错误，返回-1
+        return -1
+    else:  # 此时，第一个版本一定是正确的，最后一个版本一定是错误的
+        while left < right-1:
+            mid = (left + right) // 2
+            flag = is_bad_version(mid)
+            if flag:
+                right = mid
+            else:
+                left = mid
+        return right
+
+
+print(first_bad_version(5))
--- twenty_day/day01/704_search.py
+++ twenty_day/day01/704_search.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """ 704. 二分查找
-给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中
+给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target ，写一个函数搜索 nums 中
 的 target，如果目标值存在返回下标，否则返回 -1。
 
 示例 1:
@@ -14,3 +14,27 @@
 输出: -1
 解释: 2 不存在 nums 中因此返回 -1
 """
+
+
+def search(nums, target):
+    left = 0
+    right = len(nums) - 1
+    if target < nums[left] or target > nums[right]:
+        return -1
+    else:
+        while left != right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        if nums[left] == target:
+            return left
+        else:
+            return -1
+
+
+nums1 = [-2, 0, 3, 5, 9, 12]
+print(search(nums1, -3))
