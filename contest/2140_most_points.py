a5116a469bdf1ce2eedc70ec02e33eabe74f6405
xhw_587369 <xiang09@vip.qq.com>
2022-03-19 17:12:50 +0800

    contest 276 T3 done

--- contest/2140_most_points.py
+++ contest/2140_most_points.py
@@ -35,3 +35,11 @@ brainpoweri 个问题（即只能跳过接下来的 brainpoweri 个问题）。�
     questions[i].length == 2
     1 <= pointsi, brainpoweri <= 10^5
 """
+
+
+def most_points(questions):
+    n = len(questions)
+    dp = [0] * (n + 1)
+    for i in range(n-1, -1, -1):
+        dp[i] = max(dp[i+1], questions[i][0] + dp[min(n, i + questions[i][1] + 1)])
+    return dp[0]
