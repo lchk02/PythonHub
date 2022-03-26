ab9c7af1f8f44af5b5317ceba67889d63a205565
mox_lin <mox_lin@mox-lindeMacBook-Pro.local>
2022-03-26 10:28:53 +0800

    206 AC

---
+++ leetcode/206_reverse_list.py
@@ -0,0 +1,41 @@
+""" 206. 反转链表
+给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+
+示例 1：
+输入：head = [1,2,3,4,5]
+输出：[5,4,3,2,1]
+
+示例 2：
+输入：head = [1,2]
+输出：[2,1]
+
+示例 3：
+输入：head = []
+输出：[]
+
+提示：
+    链表中节点的数目范围是 [0, 5000]
+    -5000 <= Node.val <= 5000
+
+进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+"""
+
+from algorithm.link_list import ListNode
+
+
+def reverse_list(head):
+    if head:
+        tail, cur = head, head.next
+        while cur:
+            tail.next, cur.next = cur.next, head
+            head, cur = cur, tail.next
+    return head
+
+
+hd = ListNode.create_link_list_tail([])
+print(ListNode.traverse_link_list(reverse_list(hd)))
